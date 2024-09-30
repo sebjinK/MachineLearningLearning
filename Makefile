eb4e48a352f3971d1dd6e8ad74c4986f2bf6b6cd
sebjinK <142517865+sebjinK@users.noreply.github.com>
2024-09-29 22:45:14 -0500

    Part 2 - Modfied makefile, changed /Makefile/

--- Makefile
+++ Makefile
@@ -2,26 +2,17 @@ CC=g++
 CPPFLAGS=-Wall -Wextra -std=c++11
 # SRC=main.cpp src/data.cpp src/dataHandler.cpp 
 OBJS=dataHandler.o data.o
-TARGET=libdata.so
-
-
+TARGET=libdata
 
 all:$(TARGET)
 
-$(TARGET): libdir objdir knnobjdir obj/dataHandler.o obj/data.o KNN/obj/knn.o
-	$(CC) $(CPPFLAGS) obj/*.o -o $./lib/$@
-
-libdir: 
-	mkdir $./lib
-objdir:
-	mkdir $./obj
-knnobjdir:
-	mkdir $./KNN/obj
+$(TARGET): obj/dataHandler.o obj/data.o KNN/obj/knn.o
+	$(CC) $(CPPFLAGS) -c main.cpp obj/data.o obj/dataHandler.o KNN/obj/knn.o -o $./lib/$(TARGET)
 
 obj/data.o: ./src/data.cpp
 	$(CC) -fPIC $(CPPFLAGS) -c $< -o $@ 
 obj/dataHandler.o: ./src/dataHandler.cpp
-	$(CC) -fPIC $(CPPFLAGS) -c .$< -o $@
+	$(CC) -fPIC $(CPPFLAGS) -c $< -o $@
 KNN/obj/knn.o: ./KNN/src/knn.cpp
 	$(CC) -fPIC $(CPPFLAGS) -c $< -o $@
 
--- main.cpp
+++ main.cpp
@@ -1,13 +1,44 @@
+// #include "../src/data.cpp"
+// #include "../src/dataHandler.cpp"
+// #include "./src/knn.cpp"
+#include "./KNN/src/knn.hpp"
 #include <iostream>
-//#include "data.cpp"
-#include "dataHandler.hpp"
 
 int main()
 {
     DataHandler * dh = new DataHandler();
-    dh->readFeatureVector("t10k-images.idx3-ubyte");// read from MNIST
-    dh->readFeatureLabels("t10k-labels.idx1-ubyte"); //read from MNIST
+    dh->readFeatureVector("./images&labels/t10k-images.idx3-ubyte");// read from MNIST
+    dh->readFeatureLabels("./images&labels/t10k-labels.idx1-ubyte"); //read from MNIST
     //srand(time(0));
     dh->splitData();
     dh->countClasses();
+    KNN * knearest = new KNN();
+    knearest->setTrainingData(dh->getTrainingData());
+    knearest->setTestData(dh->getTestData());
+    knearest->setValidationData(dh->getValidationData());
+    double performance = 0;
+    double bestPerformance = 0;
+
+    int bestK = 1;
+    for (int i = 1; i <= 4; i++)
+    {
+        if (i == 1)
+        {
+            knearest->setK(i);
+            performance = knearest->validatePerformance();
+            bestPerformance = performance;
+        }
+        else
+        {
+            knearest->setK(i);
+            performance = knearest->validatePerformance();
+            if (performance > bestPerformance)
+            {
+                bestPerformance = performance;
+                bestK = i;
+            }
+        }
+    }
+    knearest->setK(bestK);
+    knearest->testPerformance();
 }
